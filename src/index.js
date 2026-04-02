04fb5475ce5d20fa5caaf1d2dd852ee6f4b79c3a
KisanVista <kishanbm22@gmail.com>
2026-04-02 09:27:33 +0530

    Fix Vercel build: use env var for Google Client ID instead of inline constant

--- src/index.js
+++ src/index.js
@@ -5,13 +5,11 @@ import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import ErrorBoundary from './components/ErrorBoundary';
 
-const GOOGLE_CLIENT_ID = '527976513968-5fpble7j1c7liiph17hp5g834qrgpnth.apps.googleusercontent.com';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
         <App />
       </GoogleOAuthProvider>
     </ErrorBoundary>
